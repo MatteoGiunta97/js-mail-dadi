cbd637accc4000dbd424152b08cb7cea4df62b8f
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-08 17:20:45 +0100

    feat: add js

--- mail/js/script.js
+++ mail/js/script.js
@@ -10,3 +10,35 @@ Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 
 */
+
+
+// Creo una lista di e-mail
+const emails = ['nome.cognome@email.1', 'nome.cognome@email.2', 'nome.cognome@email.3', 'nome.cognome@email.4', 'nome.cognome@email.5', 'nome.cognome@email.6', 'nome.cognome@email.7', 'nome.cognome@email.8', 'nome.cognome@email.9', 'nome.cognome@email.10'];
+const userEmail = 'nome.cognome@email.5';
+
+console.log(emails)
+
+let emailFound = false;
+// Scorrere l'elenco delle email
+for(let i = 0; i < emails.length; i++) {
+    const thisEmail = emails[i];
+
+    // Per ogni email:
+    // Controllo se l'email attuale nel ciclo è uguale a quella dell'utente
+    // Se trovo l'email nella lista -> mi appunto che l'ho trovata (nella viariabile)
+    if(thisEmail === userEmail) {
+        emailFound = true;
+    }
+
+    console.log(thisEmail)
+}
+
+// Alla fine del ciclo se nella variabile vedo che l'email c'è stampo "accesso consentito"
+// altrimenti stampo "accesso negato"
+if(emailFound === true) {
+    alert("accesso consentito");
+} else {
+    alert("accesso negato");
+}
+
+console.log(emailFound)
