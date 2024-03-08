b49ddc1b59f0112532271106e144e1f82c8ca413
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-08 16:34:03 +0100

    feat:  add js script (console)

--- dice-game/js/script.js
+++ dice-game/js/script.js
@@ -9,10 +9,25 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // Gioco dei dadi:
 
-    // genero un numero randomico da 1 a 6
-    // assegno il numero generato al computer
+    // genero un numero randomico da 1 a 6 e assegno il numero generato al computer
+    let computerNumber = Math.floor(Math.random() * 6) + 1; 
+    console.log('computer' + '-' + computerNumber)
 
-    // genero un numero randomico da 1 a 6
-    // assegno il numero generato al giocatore
+    // genero un numero randomico da 1 a 6 e assegno il numero generato al giocatore
+    let playerNumber = Math.floor(Math.random() * 6) + 1;
+    console.log('player' + '-' + playerNumber)
 
     // confronto i due numeri generati e dichiaro il numero più grande vincitore
+    const winner = 'wins';
+    const draw = 'draw';
+
+    if (computerNumber > playerNumber) {
+        console.log('computer ' + winner)
+
+    }else if (playerNumber > computerNumber) {
+        console.log('player ' + winner)
+        
+    }else if (computerNumber === playerNumber) {
+        console.log(draw)
+        
+    }
